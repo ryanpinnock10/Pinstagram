3f88399757dbef2462222e39f49f5c25a317721a
ryanpinnock10 <ryanpinnock10@gmail.com>
2019-11-14 02:46:21 -0500

    Parse Server running

--- Pinstagram/AppDelegate.swift
+++ Pinstagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Pinstagram"
+                configuration.server = "https://stark-reaches-40119.herokuapp.com/parse"
+            })
+        )
+        
+        
         return true
     }
 
